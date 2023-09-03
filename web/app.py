903e83d05b4510bffa363f314a28522e8a13372e
Federico <federicofarias@outlook.com>
2023-09-03 02:25:13 -0300

    update dependencies

--- web/app.py
+++ web/app.py
@@ -5,6 +5,11 @@ import pickle
 from sklearn import svm
 import streamlit as st
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib.patches import Patch
+from matplotlib.lines import Line2D
+import seaborn as sns
 
 # Path del modelo preentrenado
 MODEL_PATH = '/mount/src/tpinicial/web/models/kmeans_model.pkl'
